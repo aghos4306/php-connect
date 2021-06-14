e3dbc846920434f209cafa4e7142eadb2181a5a2
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-06-14 13:35:29 +0300

    create init to handle session, autoloader, helper fn

--- core/init.php
+++ core/init.php
@@ -1,3 +1,20 @@
-/**
-store autolaoder, start session, include global files
-*/
+<!-- store autolaoder, start session, include global files -->
+
+
+<?php
+//Start Session
+session_start();
+
+//Include Configuration
+require_once('config/config.php');
+
+//Helper Function Files
+require_once('helpers/system_helper.php');
+require_once('helpers/format_helper.php');
+require_once('helpers/db_helper.php');
+
+// Autoload classes
+function __autoload($class_name)
+{
+    require_once('libraries/' . $class_name . '.php');
+}
