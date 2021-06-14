feeeb5bbb9c40205d5f3f910d5af76da2dc633e7
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-06-14 15:27:47 +0300

    cut out header and footer and include them in frontpage

---
+++ templates/includes/footer.php
@@ -0,0 +1,63 @@
+</div>
+</div>
+</div>
+
+<div class="col-md-4">
+    <div id="sidebar">
+        <!-- block start -->
+        <div class="block">
+            <h3>Login Form</h3>
+            <form role="form">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input name="username" type="text" class="form-control" placeholder="Enter Username">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                </div>
+                <button name="do_login" type="submit" class="btn btn-primary">Login</button>
+                <a class="btn btn-default" href="register.html">Create an Account</a>
+            </form>
+        </div>
+        <!-- category start -->
+        <div class="block">
+            <h3>Categories</h3>
+            <div class="list-group">
+                <a href="#" class="list-group-item active">All Topics
+                    <span class="badge pull-right">14</span>
+                </a>
+                <a href="#" class="list-group-item">Design
+                    <span class="badge pull-right">3</span>
+                </a>
+                <a href="#" class="list-group-item">Development
+                    <span class="badge pull-right">3</span>
+                </a>
+                <a href="#" class="list-group-item">Search Engine
+                    <span class="badge pull-right">3</span>
+                </a>
+                <a href="#" class="list-group-item">Business & Marketing
+                    <span class="badge pull-right">3</span>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</div>
+</div>
+</div><!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+<script>
+    window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')
+</script>
+<script src="js/bootstrap.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+
+</html>
