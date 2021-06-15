c85c1101a7b62478c9f2fd6bbd1aa8910bbad328
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-06-15 15:57:21 +0300

    get number of replies per topic

---
+++ helpers/db_helper.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ *	Get # of replies per topic
+ */
+function replyCount($topic_id)
+{
+    $db = new Database;
+    $db->query('SELECT * FROM replies WHERE topic_id = :topic_id');
+    $db->bind(':topic_id', $topic_id);
+    //Assign Rows
+    $rows = $db->resultset();
+    //Get Count
+    return $db->rowCount();
+}
