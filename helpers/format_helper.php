e1c1034cefbbca32824027288d92141d7e76f813
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-06-15 15:45:21 +0300

    render category user and date dynamically from db

---
+++ helpers/format_helper.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/*
+ * URL Format
+ */
+function urlFormat($str)
+{
+    //Strip out all whitespace
+    $str = preg_replace('/\s*/', '', $str);
+    //Convert the string to all lowercase
+    $str = strtolower($str);
+    //URL Encode
+    $str = urlencode($str);
+    return $str;
+}
+
+/*
+ * Format Date
+ */
+function formatDate($date)
+{
+    $date = date("F j, Y, g:i a", strtotime($date));
+    return $date;
+}
--- templates/frontpage.php
+++ templates/frontpage.php
@@ -7,14 +7,16 @@
             <li class="topic">
                 <div class="row">
                     <div class="col-md-2">
-                        <img class="avatar pull-left" src="img/gravatar.jpg" />
+                        <img class="avatar pull-left" src="img/<?php echo $topic->avatar; ?>" />
                     </div>
                     <div class="col-md-10">
                         <div class="topic-content pull-right">
-                            <h3><a href="topic.html"><?php echo $topic->title ?></a></h3>
+                            <h3><a href="topic.html"><?php echo $topic->title; ?></a></h3>
                             <div class="topic-info">
-                                <a href="category.html">Development</a> >> <a href="profile.html">Austin Aghogho</a>
-                                <span class="badge pull-right">4</span>
+                                <a href="topics.php?category=<?php echo urlFormat($topic->category_id); ?>"><?php echo $topic->name; ?></a> >>
+                                <a href="topics.php?user=<?php echo urlFormat($topic->user_id); ?>"><?php echo $topic->username; ?></a> >>
+                                <?php echo formatDate($topic->create_date); ?>
+                                <span class="badge pull-right">3</span>
                             </div>
                         </div>
                     </div>
